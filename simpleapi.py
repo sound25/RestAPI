8643f3335729c4fd9307167cb8f0f87e97f3cd0a
sound25 <soundaryathangavelu@gmail.com>
2021-08-08 12:59:21 +0530

    My Practices

---
+++ simpleapi.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Resource,Api
+app=Flask(__name__)
+
+api=Api(app)
+
+
+puppies=[]
+class Puppy(Resource):
+	def get(self,name):
+		for pup in puppies:
+			if pup['name']==name:
+				return pup
+		return {'name':None},404
+	def post(self,name):
+		pup={'name':name}
+		puppies.append(pup)
+		return pup
+	def delete(self,name):
+		for ind,pup in enumerate(puppies):
+			if pup['name']==name:
+				deleted_pup=puppies.pop(ind)
+				return {"Note": 'delete success'}
+
+class All(Resource):
+
+	def get(self):
+		return {'puppies':puppies}
+
+api.add_resource(Puppy,'/puppy/<string:name>')
+api.add_resource(All,'/puppies')
+
+if(__name__=='__main__'):
+	app.run(debug=True)
